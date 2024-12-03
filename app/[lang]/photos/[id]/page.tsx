bb06c5830158be3491ae05b8f44f0e985413a9b5
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-12-03 13:55:18 +0600

    fix: console warning

--- app/[lang]/photos/[id]/page.tsx
+++ app/[lang]/photos/[id]/page.tsx
@@ -2,10 +2,13 @@ import PhotoDetailsComponent from "@/components/photo-details";
 import { Locale } from "@/i18n.config";
 
 const PhotoDetails = async ({
-  params: { id, lang },
+  params,
 }: {
   params: { id: string; lang: Locale };
 }) => {
+  const lang = (await params).lang;
+  const id = (await params).id;
+
   return <PhotoDetailsComponent id={id} lang={lang} />;
 };
 
